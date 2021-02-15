9c53f3546dd999c7051b14640aca3a8d9102ced0
227Mahaman <zalkiabass.456@gmail.com>
2021-02-15 20:57:26 +0100

    langues generated

--- api/config/var.php
+++ api/config/var.php
@@ -4,7 +4,7 @@
         $this->config["db_name"] = 'akoy_prestation';
         $this->config["username"] = 'root';
         $this->config["password_"] = '';
-$this->config["tables"] = ['categories_publication','cv','ecole','files','module','module_role','publications','roles','type_publication','types_user','users','villes',];
+$this->config["tables"] = ['categories_publication','cv','ecole','files','langues','module','module_role','publications','roles','type_publication','types_user','users','villes',];
 
 $this->config['tables']['categories_publication'] = ['id','titre',];
 
@@ -22,34 +22,38 @@ $this->config['tables']['files'] = ['id','file_name','file_url','file_type','fil
 
 $this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];
 
+$this->config['tables']['langues'] = ['id','code','titre',];
+
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];
+
 $this->config['tables']['module'] = ['id','name','icon','description','action_url','sub_module','is_menu','created_at','updated_at','statut','user_create',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];
 
 $this->config['tables']['module_role'] = ['id','role_id','module_id','create_at',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];
 
 $this->config['tables']['publications'] = ['id','type_publication','category_publication','titre','description','lieu','date_annee_bourse','date_debut','date_fin','ecole','date_elaboration','file','user_create','create_at','updated_at','statut',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];
 
 $this->config['tables']['roles'] = ['id','name','description',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];
 
 $this->config['tables']['type_publication'] = ['id','titre',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];
 
 $this->config['tables']['types_user'] = ['id','label',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];
 
 $this->config['tables']['users'] = ['id','first_name','last_name','phone_number','type_user','created_at','updated_at','photo','role','password_','statut',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];$this->config['tables']['users']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];$this->config['tables']['users']['id'] = ['id'];
 
 $this->config['tables']['villes'] = ['id','titre',];
 
-$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];$this->config['tables']['users']['id'] = ['id'];$this->config['tables']['villes']['id'] = ['id'];
+$this->config['tables']['categories_publication']['id'] = ['id'];$this->config['tables']['cv']['id'] = ['id'];$this->config['tables']['ecole']['id'] = ['id'];$this->config['tables']['files']['id'] = ['id'];$this->config['tables']['langues']['id'] = ['id'];$this->config['tables']['module']['id'] = ['id'];$this->config['tables']['module_role']['id'] = ['id'];$this->config['tables']['publications']['id'] = ['id'];$this->config['tables']['roles']['id'] = ['id'];$this->config['tables']['type_publication']['id'] = ['id'];$this->config['tables']['types_user']['id'] = ['id'];$this->config['tables']['users']['id'] = ['id'];$this->config['tables']['villes']['id'] = ['id'];
